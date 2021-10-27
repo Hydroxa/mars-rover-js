9c262775b9d54a570b25923a9295390ad931dc0e
Hydroxa <alexgillott489@gmail.com>
2021-10-27 16:26:49 +0100

    Added visuals with Kit and added Camera page

--- src/frontend/pages/App.js
+++ src/frontend/pages/App.js
@@ -1,19 +1,21 @@
-import React from 'react';
-import { Route, Switch } from 'react-router-dom';
-import Home from './home/Home';
-import Login from './login/Login';
+import React from "react";
+import { Route, Switch } from "react-router-dom";
+import Home from "./home/Home";
+import Login from "./login/Login";
 import Photos from "./Photos/Photos";
-import './css/cssreset.css';
-import './css/App.css';
-import './css/Utilities.css';
-import './css/Header.css';
+import "./css/cssreset.css";
+import "./css/App.css";
+import "./css/Utilities.css";
+import "./css/Header.css";
+import Camera from "./Photos/Camera/Camera";
 
 export default function App() {
-  return (
-    <Switch>
-      <Route exact path="/" component={Home} />
-      <Route exact path="/login" component={Login} />
-      <Route exact path="/photos" component={Photos} />
-    </Switch>
-  );
+    return (
+        <Switch>
+            <Route exact path="/" component={Home} />
+            <Route exact path="/login" component={Login} />
+            <Route exact path="/photos" component={Photos} />
+            <Route exact path="/photos/camera" component={Camera} />
+        </Switch>
+    );
 }
---
+++ src/frontend/pages/Photos/Camera/Camera.js
@@ -0,0 +1,33 @@
+import React, { useEffect, useState } from "react";
+import { useLocation } from "react-router-dom";
+import "../../App.css";
+import "./Camera.css";
+
+function useQuery() {
+    return new URLSearchParams(useLocation().search);
+}
+
+export default function Camera(props) {
+    const query = useQuery();
+    const camera = query.get("camera");
+    const rover = query.get("rover");
+
+    function goToRovers() {
+        props.history.push("/photos");
+    }
+
+    //fetch("/api/rovers/")
+    const roverSol = 3000;
+    return (
+        <>
+            <div id="controlPanel">
+                <button className="backbutton" onClick={goToRovers}>
+                    Back to Rovers
+                </button>
+                <div id="sliderContainer">
+                    Sol<input type="range" min="2" max={roverSol} className="slider" id="sol"></input>
+                </div>
+            </div>
+        </>
+    );
+}
