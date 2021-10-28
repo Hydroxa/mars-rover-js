590f121856455fe4619026a4acb02a0cef08697a
Anya Murphy <anya.murphy@mac.com>
2021-10-28 14:54:02 +0100

    Updated quiz table

--- src/backend/sqlScripts/5_create_quiz_database.sql
+++ src/backend/sqlScripts/5_create_quiz_database.sql
@@ -1,9 +1,5 @@
 CREATE TABLE quiz (
 	id SERIAL PRIMARY KEY,
 	question varchar(150),
-	correct_answer int,
-	answer_one varchar(50),
-	answer_two varchar(50),
-	answer_three varchar(50),
-	answer_four varchar(50)
-)
+	true_or_false int
+);
