089dd9119ff441b60aea2467aae124e40a62c547
JabrilMahamud <91251262+JabrilMahamud@users.noreply.github.com>
2021-10-28 10:49:20 +0100

    unhandled promise rejection

--- src/backend/controllers/RegistrationController.js
+++ src/backend/controllers/RegistrationController.js
@@ -2,17 +2,36 @@ import express from "express";
 import UserService from "../services/UserService";
 import * as UserRepository from "../repositories/UserRepository";
 import { secret } from "../config";
+import database from "../repositories/database";
+import { errors } from "pg-promise";
 
 const router = express.Router();
 
 router.post("/", async (req, res) => {
   const body = req.body;
-
   const passHash = UserService.hashString(body.password);
-
-  await UserRepository.addUserToDatabase(body.name, body.username, body.email, passHash);
+  try {
+    checkExistance(body.name)
+  } catch (error) {
+    res.send(errors)
+  }
+  await UserRepository.addUserToDatabase(
+    body.name,
+    body.username,
+    body.email,
+    passHash
+  );
 
   res.sendStatus(201);
 });
+
+function checkExistance(name) {
+  const search = database.any(`SELECT username FROM users WHERE preferred_name = ${name}`)
+  if (name != search) {
+    return true;
+  } else {
+    res.send("Username exists");
+  }
+}
 
 export default router;
